49f3aa1a4de78fe1534218e9172d8a3c00d8ad69
thedantonn <138753839+thedantonn@users.noreply.github.com>
2023-07-26 12:15:02 +0530

    Updates

---
+++ precache-manifest.ab0b70c7782f99ad92a6cf024c4198c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ed4c8dad53232607885",
+    "url": "/TheBlackMAMBA/static/css/main.09ff67c8.chunk.css"
+  },
+  {
+    "revision": "5ed4c8dad53232607885",
+    "url": "/TheBlackMAMBA/static/js/main.5ed4c8da.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/TheBlackMAMBA/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "b4cfdddb7105565c8700",
+    "url": "/TheBlackMAMBA/static/js/2.b4cfdddb.chunk.js"
+  },
+  {
+    "revision": "aed84ea981857ca2756c",
+    "url": "/TheBlackMAMBA/static/js/runtime~main.aed84ea9.js"
+  },
+  {
+    "revision": "1112efa0afb3e0d6c19c9585badcc1a4",
+    "url": "/TheBlackMAMBA/index.html"
+  }
+];
